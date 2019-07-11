c317d1dba8ff805fb9e0b7b3e250178cbecf9ef8
doctordulitel <doctordulitel@gmail.com>
2019-07-11 10:48:30 +0100

    Primer Commit del Backend-server

---
+++ app.js
@@ -0,0 +1,31 @@
+// Requires
+var express = require('express');
+var mongoose = require('mongoose');
+
+
+//Inicializar variables
+var app = express();
+
+
+//Conexión a la Base de Datos
+mongoose.connection.openUri('mongodb://localhost:27017/crokisdb', (err, res) => {
+    if (err) throw err;
+
+    console.log('Base de Datos: \x1b[32m%s\x1b[0m', 'online');
+})
+
+
+
+// Rutas
+app.get('/', (rep, res, next) => {
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente'
+    })
+});
+
+
+//Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
