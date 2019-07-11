c86c22b27c05b996a145beebd8bacfeee2229c05
doctordulitel <doctordulitel@gmail.com>
2019-07-11 22:41:45 +0100

    usuario y token listo

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
 
 //Inicializar variables
 var app = express();
+
+
+//Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+//Importar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 
 //Conexión a la Base de Datos
@@ -15,14 +27,11 @@ mongoose.connection.openUri('mongodb://localhost:27017/crokisdb', (err, res) => 
 })
 
 
+//Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
-// Rutas
-app.get('/', (rep, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    })
-});
 
 
 //Escuchar peticiones
